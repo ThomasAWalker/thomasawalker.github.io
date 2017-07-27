807b24fba52fa0db190cfad3cd49a6ac1e68f44e
tom <tom@tom-walker.co.uk>
2017-07-27 16:33:16 +0100

    support more contact details

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.Trello.authorize({
         type: 'popup',
-        name: 'Getting Started Application',
+        name: 'Novice group email list',
         scope: {
                 read: 'true',
                 write: 'true' },
@@ -8,6 +8,18 @@ window.Trello.authorize({
                 success: authenticationSuccess,
                 error: authenticationFailure
 });
+
+
+function authenticationSuccess() {
+        console.log('authenticated');
+        window.Trello.get('/members/me/boards').then(function(boards) {
+                boards.filter(b=>b.name == 'Tuesday Evening Groups').forEach(processBoard)
+        });
+}
+
+function authenticationFailure() {
+        console.log('failed to authenticate');
+}
 
 function processBoard(b) {
         console.log('processing board',b);
@@ -17,11 +29,11 @@ function processBoard(b) {
 
 function processLists(lists) {
         console.log(lists);
-        var isEmail = l => l.name === 'Email Lists';
-        var email = lists.filter(isEmail)[0];
+        var isEmailList = l => l.name === 'Email Lists';
+        var email = lists.filter(isEmailList)[0];
 
         function include(l) {
-                return (!isEmail(l));
+                return (!isEmailList(l));
         }
 
         var cardDel = processEmail(email);
@@ -31,54 +43,78 @@ function processLists(lists) {
         });
 
         function processEmail(l) {
-                return window.Trello.get('/lists/'+l.id+'/cards').then(function(cs) {
-                        var dels = cs.map(c => window.Trello.del('/cards/'+c.id));
+                return window.Trello.get('/lists/'+l.id+'/cards').then(function(cards) {
+                        var dels = cards.map(c => window.Trello.del('/cards/'+c.id));
                         return Promise.all(dels);
                 });
         }
 
         function processList(l) {
-                window.Trello.get('/lists/'+l.id+'/cards').then(function(cs) {
+                window.Trello.get('/lists/'+l.id+'/cards').then(function(cards) {
                         var emails = [];
-                        cs.forEach(function(c) {
-                                var email = emailFromCard(c);
+                        cards.forEach(function(c) {
+                                var email = contactDetailsFromCard(c);
                                 if(email) {
                                         emails.push(email);
                                 }
                         });
                         console.log(l.name, emails);
                         if(emails.length) {
-                                createCard(l.name, emails.join('\n'), l.pos);
+                                createCard(l.name, emails.map(x=>x.displayEmail).join('\n'), l.pos);
                         }
                 });
-        }
 
-        function createCard(name, desc, pos) {
-                json = {
-                        name : name,
-                        desc : desc,
-                        idList : email.id,
-                        pos : pos
-                };
-                window.Trello.post('/cards', json);
-        }
-}
-
-function emailFromCard(c) {
-        var r = /([^< ",]*@[^> ",]*)/; 
-        var m = r.exec(c.name);
-        if(m) {
-                return m[0];
+                function createCard(name, desc, pos) {
+                        json = {
+                                name : name,
+                                desc : desc,
+                                idList : email.id,
+                                pos : pos
+                        };
+                        console.log('creating',json);
+                        window.Trello.post('/cards', json);
+                }
         }
 }
 
-function authenticationSuccess() {
-        console.log('succ');
-        window.Trello.get('/members/me/boards').then(function(boards) {
-                boards.filter(b=>b.name == 'Tuesday Evening Groups').forEach(processBoard)
+function ContactDetails(email, name) {
+        this.email = email;
+        this.name = name;
+
+        Object.defineProperty(this, 'displayEmail', {
+                get : function() {
+                        var em;
+                        if(this.name) {
+                                em = this.name.trim() + ' <'+this.email+'>';
+                        }
+                        else {
+                                em = this.email;
+                        }
+                        return '    '+em;
+                }
         });
 }
 
-function authenticationFailure() {
-        console.log('fail');
+
+function contactDetailsFromCard(c) {
+        var nre = /([^<]*)<([^< ]*@[^> ]*)/;
+        var m = nre.exec(c.name);
+        if(m) {
+                return new ContactDetails(m[2], m[1]);
+        }
+        var r = /([^< ",]*@[^> ",]*)/; 
+        m = r.exec(c.name);
+        if(m) {
+                return new ContactDetails(m[1]);
+        }
+}
+
+
+function a() {
+        var self = this;
+        this.p = new Promise(function(res, rej) {
+                self.accept = res;
+                self.fail = rej;
+        });
+
 }
